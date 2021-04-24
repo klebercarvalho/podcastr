bc7ce71ed311d8834dc366cfde2e0babcb342027
Kleber Rodrigo de Carvalho <kleber.carvalho@ibm.com>
2021-04-24 12:08:49 -0300

    add player component

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss';
+
+
+export function Player() {
+
+  return (
+    <div className={styles.playerContainer}>
+
+      <header>
+        <img src="/playing.svg" alt="Tocando agora" />
+        <strong>Tocando agora</strong>
+      </header>
+
+      <div className={styles.emptyPlayer}></div>
+      <strong>
+        Selecione um podcast para ouvir
+      </strong>
+
+    </div>
+  )
+}
